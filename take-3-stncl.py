93706efb42aa832e6f2efb256921f143a6ff55d7
B4rxu5 <157962283+B4rxu5@users.noreply.github.com>
2024-02-29 11:20:04 +0545

    Add files via upload

---
+++ take-3-stncl.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from pylab import *
+import matplotlib.pyplot as plt
+
+# read image to array
+im = array(Image.open('Tattoo-designs/Bondage-skull.jpg').convert('L'))
+
+# create a new figure
+figure()
+
+# show contours with origin upper left corner
+contour(im, levels=[160], colors='black', origin='image')
+axis('equal')
+
+plt.savefig("Tattoo_stencils\Take-3-stencil.jpg")
